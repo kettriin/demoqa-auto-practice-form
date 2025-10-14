3fad88f8ccca8b11734b9270fec161b60b92ca61
kettriin <e.shch1904@gmail.com>
2025-10-14 16:03:52 +0300

    Проверка соответствия введенных и сохраненных данных убрана из постусловия в тело теста. Для всех введенных данных проверяем теперь соответствие сохраненным данным по ячейкам в результирующей таблице.

--- src/test/java/FillFormTest.java
+++ src/test/java/FillFormTest.java
@@ -1,5 +1,4 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,23 +12,6 @@ public class FillFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-    }
-
-    @AfterEach
-    void checkResultTable() {
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").shouldHave(
-                text("Marry By"),
-                text("mbemail@mail.ru"),
-                text("Female"),
-                text("9384109813"),
-                text("01 November,1991"),
-                text("Biology"),
-                text("Sports, Reading, Music"),
-                text("png.png"),
-                text("Stretty Street 128"),
-                text("NCR Noida")
-        );
     }
 
     @Test
@@ -69,6 +51,20 @@ public class FillFormTest {
         $("#currentAddress").setValue("Stretty Street 128");
 
         $("#submit").click();
+
+        // проверка на овпадение введенных и сохраненных данных в модалке
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        // через проверку содержимого нужных нам ячеек в таблице
+        $(".table-responsive").$("tbody tr", 0).$("td", 1).shouldHave(text("Marry By"));
+        $(".table-responsive").$("tbody tr", 1).$("td", 1).shouldHave(text("mbemail@mail.ru"));
+        $(".table-responsive").$("tbody tr", 2).$("td", 1).shouldHave(text("Female"));
+        $(".table-responsive").$("tbody tr", 3).$("td", 1).shouldHave(text("9384109813"));
+        $(".table-responsive").$("tbody tr", 4).$("td", 1).shouldHave(text("01 November,1991"));
+        $(".table-responsive").$("tbody tr", 5).$("td", 1).shouldHave(text("Biology"));
+        $(".table-responsive").$("tbody tr", 6).$("td", 1).shouldHave(text("Sports, Reading, Music"));
+        $(".table-responsive").$("tbody tr", 7).$("td", 1).shouldHave(text("png.png"));
+        $(".table-responsive").$("tbody tr", 8).$("td", 1).shouldHave(text("Stretty Street 128"));
+        $(".table-responsive").$("tbody tr", 9).$("td", 1).shouldHave(text("NCR Noida"));
 
     }
 }
